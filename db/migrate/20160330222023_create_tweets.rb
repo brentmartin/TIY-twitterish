87af0071eec75311d02c2fe38495b1ee56657656
Brent Martin <bmart35@gmail.com>
2016-03-31 08:47:12 -0500

    Initialized files
    added user.rb and tweet.rb models
    
    added user_controller.rb and tweets_controller.rb controllers

---
+++ db/migrate/20160330222023_create_tweets.rb
@@ -0,0 +1,8 @@
+# class CreateTweets < ActiveRecord::Migration
+#   def change
+#     create_table :tweets do |t|
+#       t.string :message, null: false
+#       t.belongs_to :user, index: true, foreign_key: true #user_id
+#     end
+#   end
+# end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 
+#
+# 10.times do
+#   user = User.create(name:Faker::Name.name, location:Faker::Address.city)
+#   10.times do
+#     Tweet.create(message:Faker::Lorem.sentence, user_id:user.id)
+#   end
+# end
