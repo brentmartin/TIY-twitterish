4914264a960a8bc40bb9c62c65846946d8c86ea7
Brent Martin <bmart35@gmail.com>
2016-03-31 08:53:46 -0500

    created TweetsController class in controllers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,7 @@
-# class TweetsController < ApplicationController
-#   def index
-#     @tweets = Tweet.all
-#     render template: "tweets/index.html.erb"
-#   end
-#
-# end
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+    render template: "tweets/index.html.erb"
+  end
+
+end
