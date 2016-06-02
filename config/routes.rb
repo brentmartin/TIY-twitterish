f328225675203285fbb4e77ac723551f75626049
Brent Martin <bmart35@gmail.com>
2016-06-02 18:32:05 -0500

    added in resources for both tweets and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get "/tweets", to: "tweets#index"
+  resources :tweets
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
